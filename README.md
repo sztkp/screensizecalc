7d505ec558aa5f4c8cecebfb94384bf242bc12d8
sztkp <szpeti16@gmail.com>
2017-02-26 15:19:08 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# screensizecalc
+# ScreenSizeCalc
+*Copyright Peter Szentkiralyi, 2017*
+
+*Distributed under GPLv3 license.*
+
+WIP
---
+++ screensizecalc.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+#program info
+__title__ = "Screen Size Calculator"
+__author__ = "Peter Sz."
+__licence__ = "GPLv3"
+__version__ = "0.1"
+
+#modules
+import math
+import time
+
+#static values
+inch = 2.54
+query = "Input screen diameter in inches.\n"
+
+
+def welcome():
+    #Welcome message
+    print "Welcome to %s v%s." % (__title__, __version__)
+    time.sleep(1)
+    print "Directions:"
+    time.sleep(1)
+    print "Enter values using your keyboard and press ENTER to submit.\n"
+    time.sleep(1)
+ 
+def cont():
+    #Asks if the user wants to continue (Y/n).
+    print
+    answer = raw_input("Do you want another calculation? (Y/n)")
+    if answer.lower() == "y" or answer == "":
+        selectratio()
+    elif answer.lower() == "n":
+        exit()
+    else:
+        cont() 
+
+def fourthree():
+    print
+    c = float(raw_input(query))
+    x = c / 5
+    print "Screen size:\n%s cm\n%s cm" % (round(x * 4 * inch, 1), 
+                                           round(x * 3 * inch, 1))
+    cont()
+
+def sixteennine():
+    print
+    c = float(raw_input(query))
+    x = c / math.sqrt(337)
+    print "Screen size:\n%s cm\n%s cm" % (round(x * 16 * inch, 1), 
+                                           round(x * 9 * inch, 1))
+    cont()
+
+def selectratio():
+    print "Select screen ratio:"
+    print "a) 4:3 screen"
+    print "b) 16:9 screen"
+    time.sleep(1)
+    ratio = raw_input()
+    if ratio.lower() == "a":
+        fourthree()
+    elif ratio.lower() == "b":
+        sixteennine()
+    else:
+        selectratio()
+ 
+#start program
+welcome()
+selectratio()
